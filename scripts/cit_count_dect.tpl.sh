df91bb78dc37824c93985a686fc3edd81489857a
Federico Cantini <federico.cantini@lib4ri.ch>
2018-06-04 16:29:47 +0200

    Added citation count detection template

--- scripts/cit_count_dect.tpl.sh
+++ scripts/cit_count_dect.tpl.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 USER=<user>
-XMLDIR='<full_path_of_cit_count_xml_file_to_ingest>'
+IDLIST='<path_relative_to_the_working_dir_to_save_data_to>'
 
-PIDSELECT=dirlist
-PIDOPTIONS="dirname=$XMLDIR"
+PIDSELECT=name_space
+PIDOPTIONS="namespace=eawag"
 
 # The two following lines for testing purpose. Uncomment to test on a single islandora object 
 #PIDSELECT=single
 #PIDOPTIONS="pid=<islandora_objct_pid>"
 
-COMMAND=cit_count_ingest
-COMMANDOPTIONS="dirname=$XMLDIR"
+COMMAND=cit_count_detect
+COMMANDOPTIONS="idlabels=doi;scopus;pmid|idsep=,|outfile=$IDLIST|wd=$PWD"
 
 DRYRUN=true
 if [[ $1 = "dryfalse" ]]
